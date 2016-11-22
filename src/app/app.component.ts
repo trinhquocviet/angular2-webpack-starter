ca2f6ef6de2a7bc31aa3a6e6c0070bd11737865d
Viet Trinh <viettrinh271291@gmail.com>
2016-11-22 08:03:43 +0700

    remove useHash and add scss to project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,7 +13,7 @@ import { AppState } from './app.service';
   selector: 'app',
   encapsulation: ViewEncapsulation.None,
   styleUrls: [
-    './app.component.css'
+    './../styles/main.scss'
   ],
   template: `
     <nav>
